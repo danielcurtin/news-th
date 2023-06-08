5735837c8aef252c2b3266c39074011e679c8777
Daniel Curtin <dcurtin.work@gmail.com>
2023-06-08 13:22:39 -0600

    Remove unused duplicate article card mapping

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -2,9 +2,7 @@ import './Articles.css';
 
 import ArticleCard from '../ArticleCard/ArticleCard';
 
-const Articles = ({ articles }) => {
-  const articleCards = articles.map(article => <ArticleCard key={article.id} id={article.id} author={article.author} title={article.title} description={article.description} image={article.urlToImage} date={article.publishedAt} />);
-  
+const Articles = ({ articles }) => {  
   return (
     <section className='articles'>
       {articles.length ? articles.map(article => <ArticleCard key={article.id} id={article.id} author={article.author} title={article.title} description={article.description} image={article.urlToImage} date={article.publishedAt} />) : <h1>No articles found</h1>}
