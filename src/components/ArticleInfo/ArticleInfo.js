b43e6a700a5dc84b30e6aee9e26a8cc6baa002fa
Daniel Curtin <dcurtin.work@gmail.com>
2023-06-08 12:44:39 -0600

    Fix search input being unlabelled

--- src/components/ArticleInfo/ArticleInfo.js
+++ src/components/ArticleInfo/ArticleInfo.js
@@ -7,8 +7,8 @@ const ArticleInfo = ({ article }) => {
       <div className='article-details'>
         <h2>{article.title}</h2>
         <h3>{article.author || 'Author Unknown'}</h3>
-        <p>{(article.content || article.description) || 'No information found'}</p>
-        <a target='_blank' href={article.url}>{article.url || 'No link provided'}</a>
+        <p className='article-content'>{(article.content || article.description) || 'No information found'}</p>
+        {article.url ? <p className='article-link'>See the full article <a target='_blank' href={article.url}>here</a></p> : <p className='article-link'>No article link provided</p>}
       </div>
     </section>
   );
